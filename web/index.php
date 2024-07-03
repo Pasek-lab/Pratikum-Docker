3ace369219fdddaf9c7f2e19facae8d539d49251
Pasek-lab <pasek.bayu@student.undiksha.ac.id>
2024-07-04 00:02:45 +0700

    first commit

---
+++ web/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Dockerized Web App</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        table {
+            border-collapse: collapse;
+            width: 50%;
+        }
+        table, th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+        <th colspan="2" style="text-align: center;">Profil</th>
+        </tr>
+        <tr>
+            <td>Nama saya:</td>
+            <td>I Komang Pasek Bayu Atmaja</td>
+        </tr>
+        <tr>
+            <td>NIM:</td>
+            <td>2215101023</td>
+        </tr>
+        <tr>
+            <td>Prodi:</td>
+            <td>Ilmu Komputer</td>
+        </tr>
+        <tr>
+            <td>Kelas:</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>Mata Kuliah:</td>
+            <td>Sistem Distribusi</td>
+        </tr>
+    </table>
+</body>
+</html>
